0eb8bd0c31cf9b66760db05ff1b4924b24401f5f
Melissa Wong <mellymai@gmail.com>
2022-11-19 16:29:34 -0800

    adds documentation and string method

--- shape.py
+++ shape.py
@@ -7,23 +7,22 @@ class Shape(ABC):
         self.name = name
 
     def __str__(self):
-        pass
+        return f"{self.name}, area: {self.area()}, perimeter: {self.perimeter()}"
 
-    @property 
+    @property
     def name(self):
-        """Gets the name of the current shape"""
+        """Get the name of the current shape."""
         return self.name
 
     @abstractmethod
     def area(self):
-        pass
+        """Get the area of the current shape."""
 
     @abstractmethod
     def perimeter(self):
-        pass
+        """Get the perimeter of the current shape."""
 
     @abstractmethod
     def draw(self):
-        """Prints the name of the shape followed by the area and perimeter of the shape"""
-        print(
-            f"{self.name_of_shape}, area: {self.area()}, perimeter: {self.perimeter()}")
+        """Prints the name of the shape followed by the area and perimeter of the shape."""
+        print(self)
