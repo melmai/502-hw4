6460dfe31b658623f581e2daf2e0e353d6e688d6
Melissa Wong <mellymai@gmail.com>
2022-11-25 21:02:50 -0800

    updates draw() to call __str__()

--- shape.py
+++ shape.py
@@ -31,4 +31,4 @@ class Shape(metaclass=ABCMeta):
     @abstractmethod
     def draw(self):
         """Prints the name of the shape followed by the area and perimeter of the shape."""
-        pass
+        print(self)
